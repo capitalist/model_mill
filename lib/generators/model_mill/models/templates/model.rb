e10a55ae288a2f0884e562ae41455d2c34bd3b59
Joe Martinez <joe@joemartinez.name>
2011-01-26 21:00:11 -0500

    need to add a newline after each association

--- lib/generators/model_mill/models/templates/model.rb
+++ lib/generators/model_mill/models/templates/model.rb
@@ -1,6 +1,7 @@
 class <%= options[:namespace] ? options[:namespace].camelize + '::' : '' %><%= @model_name %> < <%= options[:namespace] ? options[:namespace].camelize : 'ActiveRecord' %>::Base
   attr_accessible <%= @columns.map(&:name).sort.map{|c| ":#{c}"}.join(', ') %>
 
-  <% @associations.each do |cardinality, associations| -%><% associations.each do |a| -%><%= "#{cardinality} :#{a[:name]}" %><% end %><% end %>
+  <% @associations.each do |cardinality, associations| -%><% associations.each do |a| -%><%= "#{cardinality} :#{a[:name]}" %>
+<% end %><% end %>
 end
 
