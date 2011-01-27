96eb0d71a76a2eaad6527fe4972e0bb43636bf29
Joe Martinez <joe@joemartinez.name>
2011-01-26 21:13:36 -0500

    add class name to associations if legacy provided

--- lib/generators/model_mill/models/templates/model.rb
+++ lib/generators/model_mill/models/templates/model.rb
@@ -1,7 +1,11 @@
 class <%= options[:namespace] ? options[:namespace].camelize + '::' : '' %><%= @model_name %> < <%= options[:namespace] ? options[:namespace].camelize : 'ActiveRecord' %>::Base
   attr_accessible <%= @columns.map(&:name).sort.map{|c| ":#{c}"}.join(', ') %>
 
-  <% @associations.each do |cardinality, associations| -%><% associations.each do |a| -%><%= "#{cardinality} :#{a[:name]}" %>
+<% @associations.each do |cardinality, associations|
+  -%><% associations.each do |a|
+  -%><%= "  #{cardinality} :#{a[:name]}" +
+         (options[:namespace] ? ", :class_name => '#{options[:namespace].camelize}::#{a[:name].singularize.camelize}'" : '')
+  %>
 <% end %><% end %>
 end
 
